267efeb21e9e3252ea9b32774587da1a500e8980
Niv <nivinviraj97@gmail.com>
2022-11-18 17:48:09 +0530

    Finalizing

--- partB.c
+++ partB.c
@@ -2,6 +2,7 @@
 #include <sys/msg.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +24,7 @@ int main()
   Marks data[100];
   float totalMarks[100];
   pid_t c1, c2, cc1, cc2;
+  int belowTwntyFive = 0;
 
   FILE *file = fopen(filePath, "r");
 
@@ -39,6 +41,11 @@ int main()
     {
       data[i] = mark;
       totalMarks[i] = getTotal(mark);
+
+      if (totalMarks[i] < 25)
+      {
+        belowTwntyFive;
+      }
     }
   }
 
@@ -61,10 +68,67 @@ int main()
       break;
 
     // CC1
-    case 0:
+    case 0:;
+      sleep(0.001);
+      float t4;
+      t4 = 0;
+
+      for (int i = 60; i < 80; i++)
+      {
+        t4 += totalMarks[i];
+      }
+
+      key_t key4 = ftok("file.txt", 30);
+
+      int mqID = msgget(key4, 0);
+      if (mqID == -1)
+      {
+        printf("CC1 mq error\n");
+        perror("MQ: ");
+        printf("Error number is: %d", errno);
+        exit(0);
+      }
+
+      message m4 = createMsg(1, t4);
+      msgsnd(mqID, &m4, sizeof(t4), IPC_NOWAIT);
+
+      break;
 
     // C1
-    default:
+    default:;
+      sleep(0.001);
+      float t2;
+      t2 = 0;
+
+      for (int i = 20; i < 40; i++)
+      {
+        t2 += totalMarks[i];
+      }
+
+      key_t key2 = ftok("file.txt", 30);
+
+      int mqID2 = msgget(key2, 0);
+      if (mqID2 == -1)
+      {
+        printf("C2 message queue create error\n");
+        perror("MQ: ");
+        printf("Error number is: %d", errno);
+        exit(0);
+      }
+
+      if (mqID2 == -1)
+      {
+        printf("C1 message send error\n");
+        perror("MQ: ");
+        printf("Error number is: %d", errno);
+        exit(0);
+      }
+
+      message m1 = createMsg(1, t2);
+      msgsnd(mqID2, &m1, sizeof(t2), IPC_NOWAIT);
+
+      waitpid(cc1, NULL, 0);
+
       break;
     }
 
@@ -82,6 +146,7 @@ int main()
 
     // C2 and CC2
     case 0:
+      sleep(0.001);
       cc2 = fork();
 
       switch (cc2)
@@ -91,21 +156,82 @@ int main()
         break;
 
       // CC2
-      case 0:
+      case 0:;
+        sleep(0.001);
+        float t5 = 0;
+
+        for (int i = 80; i < 100; i++)
+        {
+          t5 += totalMarks[i];
+        }
+
+        key_t key5 = ftok("file.txt", 30);
+
+        int mqID5 = msgget(key5, 0);
+        if (mqID5 == -1)
+        {
+          printf("C5 message queue create error\n");
+          perror("MQ: ");
+          printf("Error number is: %d", errno);
+          exit(0);
+        }
+
+        if (mqID5 == -1)
+        {
+          printf("CC2 message send error\n");
+          perror("MQ: ");
+          printf("Error number is: %d", errno);
+          exit(0);
+        }
+
+        message m5 = createMsg(1, t5);
+        msgsnd(mqID5, &m5, sizeof(t5), IPC_NOWAIT);
+
         break;
 
       // C2
-      default:
+      default:;
+        sleep(0.001);
+        float t3 = 0;
+
+        for (int i = 40; i < 60; i++)
+        {
+          t3 += totalMarks[i];
+        }
+
+        key_t key3 = ftok("file.txt", 30);
+
+        int mqID3 = msgget(key3, 0);
+        if (mqID3 == -1)
+        {
+          printf("C2 message queue create error\n");
+          perror("MQ: ");
+          printf("Error number is: %d", errno);
+          exit(0);
+        }
+
+        if (mqID3 == -1)
+        {
+          printf("C2 message send error\n");
+          perror("MQ: ");
+          printf("Error number is: %d", errno);
+          exit(0);
+        }
+
+        message m3 = createMsg(1, t3);
+        msgsnd(mqID3, &m3, sizeof(t3), IPC_NOWAIT);
+        waitpid(cc2, NULL, 0);
         break;
       }
 
       break;
 
     // P
-    default:
+    default:;
       key_t key1 = ftok("file.txt", 30);
 
-      int mqID = msgget(key1, IPC_CREAT | IPC_EXCL | 0666);
+      // creating the message queue
+      int mqID = msgget(key1, 0666);
 
       if (mqID == -1)
       {
@@ -116,6 +242,35 @@ int main()
       }
       else
       {
+        float t1 = 0;
+        float subtotal = 0;
+
+        for (int i = 0; i < 20; i++)
+        {
+          t1 += totalMarks[i];
+        }
+
+        waitpid(c2, NULL, 0);
+
+        message receivedMessages[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+          int x = msgrcv(mqID, receivedMessages + i, sizeof(float), 0, IPC_NOWAIT);
+          if (x == -1)
+          {
+            printf("Parent message queue read error\n");
+            perror("MQ: ");
+            printf("Error number is: %d", errno);
+            exit(0);
+          }
+          subtotal = subtotal + receivedMessages[i].data;
+        }
+
+        subtotal += t1;
+
+        printf("The average is %f\n", subtotal / 100);
+        printf("Number of students below 25: %d\n", belowTwntyFive);
       }
 
       break;
